aa302f9f5c627841a091bb5fdf935221f06618b7
famgz <famgz@proton.me>
2024-05-30 17:38:15 -0300

    feat: add exercise conta-banco

--- java-basico/conta-banco/src/ContaTerminal.java
+++ java-basico/conta-banco/src/ContaTerminal.java
@@ -1,8 +1,34 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
 
+        int numero;
+        String agencia;
+        String nomeCliente;
+        float saldo;
+
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US);
+
+        System.out.println("Digite numero conta: ");
+        numero = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Digite numero agencia: ");
+        agencia = scan.nextLine();
+
+        System.out.println("Digite nome do cliente: ");
+        nomeCliente = scan.nextLine();
+
+        System.out.println("Digite saldo da conta: ");
+        saldo = scan.nextFloat();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %f já está disponível para saque.",
+                nomeCliente, agencia, numero, saldo);
+
+        scan.close();
     }
 }
