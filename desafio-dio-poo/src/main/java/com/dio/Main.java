2e04bad9505629c0755527ea5472488a02c71647
famgz <famgz@proton.me>
2024-06-27 10:51:36 -0300

    refactor: add abstract class Conteudo

--- desafio-dio-poo/src/main/java/com/dio/Main.java
+++ desafio-dio-poo/src/main/java/com/dio/Main.java
@@ -9,13 +9,11 @@ public class Main {
     public static void main(String[] args) {
 
         Curso curso1 = new Curso();
-
         curso1.setTitulo("Curso Java");
         curso1.setDescricao("descricao curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-
         curso2.setTitulo("Curso JS");
         curso2.setDescricao("descricao curso JS");
         curso2.setCargaHoraria(4);
