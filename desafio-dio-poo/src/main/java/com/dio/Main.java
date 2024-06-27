2e7173cf0f9b03707bf474317df586928518e3fa
famgz <famgz@proton.me>
2024-06-27 10:21:39 -0300

    feat: add initial desafio dio poo

---
+++ desafio-dio-poo/src/main/java/com/dio/Main.java
@@ -0,0 +1,33 @@
+package com.dio;
+
+import java.time.LocalDate;
+
+import com.dio.desafio.dominio.Curso;
+import com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("descricao curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+    }
+}
