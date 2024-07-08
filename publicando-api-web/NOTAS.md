fc460834d099dae69fc7951133a37e603fbaf13f
famgz <famgz@proton.me>
2024-07-08 10:30:12 -0300

    adicionado controller, primeiros endpoints e documentacao da api

--- publicando-api-web/NOTAS.md
+++ publicando-api-web/NOTAS.md
@@ -8,3 +8,6 @@ h2 client (studio)
 http://localhost:8080/h2-console
 -change url and username according to the yml file
 -connect
+
+Swagger api doc
+http://localhost:8080/swagger-ui/index.html
---
+++ publicando-api-web/dev-week/src/main/java/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package dio.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dio.domain.model.User;
+import dio.domain.repository.UserRepository;
+import dio.service.UserService;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This account number already exists");
+        }
+        return userRepository.save(userToCreate);
+    }
+
+}
