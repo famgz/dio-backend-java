fba30f7c06bf3295b64614196d1e6163243450e3
famgz <famgz@proton.me>
2024-07-22 19:59:14 -0300

    fix: cors adjustment

--- publicando-api-web/dev-week/src/main/java/dio/Application.java
+++ publicando-api-web/dev-week/src/main/java/dio/Application.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL") })
 @SpringBootApplication
 public class Application {
 
